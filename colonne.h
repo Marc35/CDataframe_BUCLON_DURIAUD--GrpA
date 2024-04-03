369653fa00e6807d2279777ddaf07d42a7b60637
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-04-03 16:14:43 +0200

    Update colonne.h
    for including all types

--- colonne.h
+++ colonne.h
@@ -4,21 +4,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+enum enum_type
+{
+    NULLVAL = 1 , UINT, INT, CHAR, FLOAT, DOUBLE, STRING, STRUCTURE
+};
+typedef enum enum_type ENUM_TYPE;
+
+union column_type{
+    unsigned int uint_value;
+    signed int int_value;
+    char char_value;
+    float float_value;
+    double double_value;
+    char* string_value;
+    void* struct_value;
+};
+typedef union column_type COL_TYPE ;
+
 typedef struct{
     char *titre;
-    int TP;
-    int TL;
-    int *Tab;
+    unsigned int TP;
+    unsigned int TL;
+    ENUM_TYPE colonne_type;
+    COL_TYPE **data;
+    unsigned long long int *index;
 }COLUMN;
 
-COLUMN *create_column(char* title);
-int insert_value(COLUMN* col, int value);
-void delete_column(COLUMN **col);
-void print_col(COLUMN* col);
-int nb_occ(COLUMN *col, int x);
-int give_val(COLUMN *col, int x);
-int supp_x(COLUMN *col, int x);
-int inf_x(COLUMN *col, int x);
-
-
-#endif //PROJECT_COLONNE_H
+COLUMN *create_column(ENUM_TYPE type, char* title);
+int insert_value(COLUMN* col, void *value);
