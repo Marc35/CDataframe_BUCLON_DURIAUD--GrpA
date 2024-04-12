acec506648034c7109b0ebd4446338f5294dfb5f
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-04-12 15:13:42 +0200

    Update cd.h
    add function create and delete

--- cd.h
+++ cd.h
@@ -1,4 +1,13 @@
-#ifndef PROJECT_CD_H
-#define PROJECT_CD_H
+#ifndef FINALPROJECT_CD_H
+#define FINALPROJECT_CD_H
 
-#endif //PROJECT_CD_H
+#include "liste.h"
+#include "colonne.h"
+
+typedef list CDATAFRAME;
+
+CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size);
+void delete_cdataframe(CDATAFRAME **cdf);
+
+
+#endif //FINALPROJECT_CD_H
