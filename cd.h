cd97e31dabde6a626974c911736c181bec413673
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-04-12 16:23:06 +0200

    Update cd.h
    Add function delete colonne and get nb col

--- cd.h
+++ cd.h
@@ -8,6 +8,8 @@ typedef list CDATAFRAME;
 
 CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size);
 void delete_cdataframe(CDATAFRAME **cdf);
+void delete_column_cdatafram(CDATAFRAME *cdf, char *col_name);
+int get_cdataframe_cols_size(CDATAFRAME *cdf);
 
 
 #endif //FINALPROJECT_CD_H
