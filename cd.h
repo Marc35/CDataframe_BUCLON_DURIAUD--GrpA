79023ecb501b1ab0b4a57a87d080942a38cc623d
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-05-08 00:56:20 +0200

    Update cd.h
    Add a lot of functions

--- cd.h
+++ cd.h
@@ -3,6 +3,8 @@
 
 #include "liste.h"
 #include "colonne.h"
+#include "help_functions.h"
+
 
 typedef list CDATAFRAME;
 
@@ -10,6 +12,10 @@ CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size);
 void delete_cdataframe(CDATAFRAME **cdf);
 void delete_column_cdatafram(CDATAFRAME *cdf, char *col_name);
 int get_cdataframe_cols_size(CDATAFRAME *cdf);
-
+void insert_col_cdataframe(CDATAFRAME *cdf, ENUM_TYPE type, char *titre, int pos_col);
+void insert_values_cdataframe(CDATAFRAME *cdf, char *titre);
+void print_cdataframe(CDATAFRAME*cdf, int col_deb, int col_fin, int ligne_deb, int ligne_fin);
+void print_name_col_cdataframe(CDATAFRAME *cdf, int col_deb, int col_fin);
+void rename_col(CDATAFRAME *cdf, char *titre);
 
 #endif //FINALPROJECT_CD_H
