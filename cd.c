2e2a3c325c96f871a96c569c2c85f50fcf131a74
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-05-10 00:56:54 +0200

    Update cd.c
    Add functions

--- cd.c
+++ cd.c
@@ -100,6 +100,22 @@ int get_cdataframe_cols_size(CDATAFRAME *cdf)
     return cpt;
 }
 
+int get_cdataframe_nb_lignes(CDATAFRAME *cdf)
+{
+    int max=0;
+    MAILLON *temp = cdf->head;
+    while (temp != NULL)
+    {
+        int cpt = temp->data->TL;
+        if (cpt > max)
+        {
+            max = cpt;
+        }
+        temp = temp->next;
+    }
+    return max;
+}
+
 void insert_values_cdataframe(CDATAFRAME *cdf, char *titre)
 {
     MAILLON *temp = cdf->head;
@@ -130,19 +146,23 @@ void insert_values_cdataframe(CDATAFRAME *cdf, char *titre)
 
     char rep[50];
     viderBuffer();      // Pour vider le buffer sinon fgets ne marchera pas correctement
+    int index_pos;
     do
     {
-        printf("Pressez Entré sans renseigner de valeur pour finir l'insertion de valeur\n");
+        printf("A quelle position voulez vous insérer votre valeur ? Tapez -1 pour l'insérer a la fin\n");
+        scanf(" %d", &index_pos);
+        viderBuffer();
+        printf("Pressez Entrée sans renseigner de valeur pour finir l'insertion de valeur\n");
         printf("Quelle valeur voulez-vous ajouter ?\n");
         fgets(rep, 50, stdin);              // On demande a l'utilisateur de rentrer sa valeur qu'on considère toujours comme un str au début
         if(strcmp(rep, "\n") != 0) {
             if (strcmp(rep, "NULL") == 0)       // Si il a rentré NULL, il y a pas besoin de convertire cette valeure
             {
-                insert_value(col, NULL);
+                insert_value(col, NULL, index_pos);
             } else    // Sinon on convertie la chaine de caractère rentrée par l'utilisateur dans le type de la colone à laquelle il rajoute une valeur
             {
                 if (cast_val(col, val, rep) == 1)
-                    insert_value(col, val[0]);
+                    insert_value(col, val[0], index_pos);
                 else
                     printf("Valeur impossible à insérer veuillez rentrer une valeur valide\n");
             }
@@ -153,8 +173,10 @@ void insert_values_cdataframe(CDATAFRAME *cdf, char *titre)
 
 void print_cdataframe(CDATAFRAME*cdf, int col_deb, int col_fin, int ligne_deb, int ligne_fin)
 {
-    if (col_fin == -1)
+    if (col_fin <= -1)
         col_fin = get_cdataframe_cols_size(cdf);
+    if (ligne_fin <= -1)
+        ligne_fin = get_cdataframe_nb_lignes(cdf);
     for (int i=ligne_deb; i<ligne_fin; i++)
     {
         int j = 0;
@@ -192,7 +214,7 @@ void print_cdataframe(CDATAFRAME*cdf, int col_deb, int col_fin, int ligne_deb, i
 
 void print_name_col_cdataframe(CDATAFRAME *cdf, int col_deb, int col_fin)
 {
-    if (col_fin == -1)
+    if (col_fin <= -1)
         col_fin = get_cdataframe_cols_size(cdf);
 
     int j = 0;
@@ -244,4 +266,127 @@ void rename_col(CDATAFRAME *cdf, char *titre)
     {
         printf("Il n'y a pas de colone à ce nom dans le CDataframe\n");
     }
+}
+
+void insert_ligne_cdataframe(CDATAFRAME *cdf)
+{
+    MAILLON *temp = cdf->head;
+    int pos_lig;
+    printf("A quelle position voulez vous insérer cette nouvelle ligne ?\n");
+    scanf(" %d", &pos_lig);
+    char rep[50];
+
+    COL_TYPE **val=NULL;
+
+    if (pos_lig >= get_cdataframe_nb_lignes(cdf))
+    {
+        printf("Il n'y a pas assez de lignes dans le CDataframe, le programme va donc l'insérer à la fin \n");
+        pos_lig = -1;
+    }
+    viderBuffer();
+    while (temp != NULL)
+    {
+        val = malloc(sizeof (COL_TYPE));
+        printf("Quelle valeur voulez vous insérer dans la colonne [%s] ?\n", temp->data->titre);
+        fgets(rep, 50, stdin);
+        if (strcmp(rep, "NULL") == 0)       // Si il a rentré NULL, il y a pas besoin de convertire cette valeur
+        {
+            insert_value(temp->data, NULL, pos_lig);
+        } else    // Sinon on convertie la chaine de caractère rentrée par l'utilisateur dans le type de la colone à laquelle il rajoute une valeur
+        {
+            if (cast_val(temp->data, val, rep) == 1) {
+                printf("test1\n");
+                insert_value(temp->data, val[0], pos_lig);
+                printf("test2\n");
+            }
+            else
+                printf("Valeur impossible à insérer veuillez rentrer une valeur valide\n");
+        }
+        free(val[0]);
+        free(val);
+        //val[0] = NULL;
+        val = NULL;
+        temp = temp->next;
+    }
+}
+
+int nb_occu_value_CD(CDATAFRAME *cdf, char* value){
+    MAILLON *temp_maillon = cdf->head;
+    COLUMN *temp_col = NULL;
+    int nb_occurence = 0;
+    while(temp_maillon != NULL) {
+        temp_col = temp_maillon->data;
+        nb_occurence += nb_occ(temp_col, value, 1);
+        temp_maillon = temp_maillon->next;
+    }
+    return nb_occurence;
+}
+
+void acces_value_CDataFrame(CDATAFRAME* cdf, int pos_col, int pos_ligne){
+    MAILLON* search_col = cdf->head;
+    COLUMN* col_value = NULL;
+    if(pos_col > get_cdataframe_cols_size(cdf) || pos_ligne > get_cdataframe_nb_lignes(cdf)){
+        printf("\nLa valeur recherché n'existe pas dans le CDataFrame ! \n");
+    }
+    else
+    {
+        for(int i = 0; i < pos_col; i++){
+            search_col = search_col->next;
+        }
+        col_value = search_col->data;
+        printf("\nLa valeur pointée est %s . \nVoulez-vous la remplacer ? (oui/non)", scearch_value(col_value, pos_ligne));
+        char user_answer[3];
+        scanf(" %s", user_answer);
+        if(user_answer[0] == 'o'){
+            char* new_value = malloc(100*sizeof(char));
+            COL_TYPE **val=NULL;
+            val = malloc(sizeof (COL_TYPE));
+            do{
+                 printf("Quelle est la nouvelle valeur que vous souhaitez entrer ?");
+                 scanf(" %s", new_value);
+            }while(cast_val(col_value, val, new_value) == 0);
+            col_value->data[pos_ligne] = val[0];
+        }
+    }
+}
+
+int nb_sup_value_CD(CDATAFRAME *cdf, char* value){
+    MAILLON *temp_maillon = cdf->head;
+    COLUMN *temp_col = NULL;
+    int nb_occurence = 0;
+    while(temp_maillon != NULL) {
+        temp_col = temp_maillon->data;
+        nb_occurence += nb_inf_val(temp_col, value, 1);
+        temp_maillon = temp_maillon->next;
+    }
+    return nb_occurence;
+}
+
+int nb_inf_value_CD(CDATAFRAME *cdf, char* value){
+    MAILLON *temp_maillon = cdf->head;
+    COLUMN *temp_col = NULL;
+    int nb_occurence = 0;
+    while(temp_maillon != NULL) {
+        temp_col = temp_maillon->data;
+        nb_occurence += nb_supp_val(temp_col, value, 1);
+        temp_maillon = temp_maillon->next;
+    }
+    return nb_occurence;
+}
+
+int suppr_ligne_CD(CDATAFRAME *cdf, int pos)
+{
+    int cpt=0;
+    MAILLON *temp = cdf->head;
+    while (temp != NULL)
+    {
+        if (pos >= get_cdataframe_nb_lignes(cdf))
+            return 0;
+        else {
+            if (suppr_val_col(temp->data, pos))
+                cpt++;
+            temp = temp->next;
+        }
+    }
+    return cpt;
 }
