e54a12a1e5a407e4ddace1ab225df9c0c9cf7465
Fanto974 <144560009+Fanto974@users.noreply.github.com>
2024-05-20 14:37:29 +0200

    Update help_functions.h

--- help_functions.h
+++ help_functions.h
@@ -1,10 +1,19 @@
 #ifndef CDATAFRAME_BUCLON_DURIAUD__GRPA_MAIN_HELP_FUNCTIONS_H
 #define CDATAFRAME_BUCLON_DURIAUD__GRPA_MAIN_HELP_FUNCTIONS_H
 
-#include "colonne.h"
+#include "menu.h"
 
-ENUM_TYPE choose_type_col();
-int cast_val(COLUMN *col, COL_TYPE **val, char* value);
+ ENUM_TYPE* choose_type_col(int nb);
+int cast_val(ENUM_TYPE type, COL_TYPE **val, char* value);
 void viderBuffer();
+int compare_any_types(void *a, void *b, ENUM_TYPE type);
+void sort_insersion(COLUMN *col);
+int partition(COLUMN *col, int g, int d);
+void quicksort(COLUMN *col, int g, int d);
+void inverse_sort(COLUMN *col);
+char *get_title();
+int is_comparable(char *value, ENUM_TYPE type);
+char* get_str_type(ENUM_TYPE type);
+ENUM_TYPE get_type_with_str(char type);
 
 #endif //CDATAFRAME_BUCLON_DURIAUD__GRPA_MAIN_HELP_FUNCTIONS_H
